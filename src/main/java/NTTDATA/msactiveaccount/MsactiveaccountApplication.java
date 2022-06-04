fc53d4078b15208971060f575c1eb4504b183358
rbenitec <105337433+rbenitec@users.noreply.github.com>
2022-06-04 13:52:46 -0500

    update

---
+++ src/main/java/NTTDATA/msactiveaccount/MsactiveaccountApplication.java
@@ -0,0 +1,13 @@
+package NTTDATA.msactiveaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsactiveaccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsactiveaccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NTTDATA/msactiveaccount/MsactiveaccountApplicationTests.java
@@ -0,0 +1,13 @@
+package NTTDATA.msactiveaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsactiveaccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
